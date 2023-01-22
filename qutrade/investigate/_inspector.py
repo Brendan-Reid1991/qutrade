db3be397ae2c6cc0f9ae3dfc4a4cde3c2fcf7a43
brendan-reid91 <brendan.reid1991@gmail.com>
2023-01-22 15:31:17 +0000

    Add weekly, monthly functions

--- qutrade/investigate/_inspector.py
+++ qutrade/investigate/_inspector.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from typing import Optional
 import os
 import warnings
 
@@ -12,7 +11,8 @@ ACCESS_KEY = api()
 
 def output_warning(arg: str):
     warnings.warn(
-        f"Output type specified incorrectly: {arg}. Must be on of 'compact' or 'full', defaulting to 'full'.")
+        f"Output type specified incorrectly: {arg}."
+        " Must be on of 'compact' or 'full', defaulting to 'full'.")
 
 
 class Inspect:
@@ -56,11 +56,14 @@ class Inspect:
             self.ticker, outputsize=output)
         data.to_csv(self.pathway+f"/{self.ticker}_Intraday", index=False)
 
-    def daily(self, output: str = 'full'):
-        """Get the daily data and save it to a CSV file. Requires premium API.
+    def daily(self, adjusted: bool = True, output: str = 'full'):
+        """Get the daily data and save it to a CSV file.
+        Unadjusted data requires a premium API.
 
         Parameters
         ----------
+        adjsuted : bool, optional
+            Whether or not to access the adjusted daily data.
         output : str, optional
             How much data to take, accepted values are 'compact' or 'full'.
             By default None
@@ -78,6 +81,78 @@ class Inspect:
             output_warning(output)
             output = 'full'
 
-        data, metadata = self._time_series.get_intraday(
-            self.ticker, outputsize=output)
-        data.to_csv(self.pathway+f"/{self.ticker}_Intraday", index=False)
+        if adjusted:
+            data, metadata = self._time_series.get_daily_adjusted(
+                self.ticker, outputsize=output)
+        else:
+            data, metadata = self._time_series.get_daily(
+                self.ticker, outputsize=output)
+        suffix = "" if not adjusted else "Adjusted"
+        data.to_csv(self.pathway+f"/{self.ticker}_Daily{suffix}", index=False)
+
+    def weekly(self, adjusted: bool = True, output: str = 'full'):
+        """Get the weekly data and save it to a CSV file.
+
+        Parameters
+        ----------
+        adjusted : bool, optional
+            Whether or not to access the adjusted daily data.
+        output : str, optional
+            How much data to take, accepted values are 'compact' or 'full'.
+            By default None
+
+        Raises
+        ------
+        ValueError
+            If interval is not an accepted value.
+
+        Warning
+            If the output type is not one of 'compact' or 'full', defaults to 'full'.
+        """
+        output = "full" if output is None else output
+        if output not in ["compact", "full"]:
+            output_warning(output)
+            output = 'full'
+
+        if adjusted:
+            data, metadata = self._time_series.get_weekly_adjusted(
+                self.ticker, outputsize=output)
+        else:
+            data, metadata = self._time_series.get_weekly(
+                self.ticker, outputsize=output)
+        suffix = "" if not adjusted else "Adjusted"
+        data.to_csv(self.pathway+f"/{self.ticker}_Weekly{suffix}", index=False)
+
+    def monthly(self, adjusted: bool = True, output: str = 'full'):
+        """Get the monthly data and save it to a CSV file.
+
+        Parameters
+        ----------
+        adjusted : bool, optional
+            Whether or not to access the adjusted daily data.
+        output : str, optional
+            How much data to take, accepted values are 'compact' or 'full'.
+            By default None
+
+        Raises
+        ------
+        ValueError
+            If interval is not an accepted value.
+
+        Warning
+            If the output type is not one of 'compact' or 'full', defaults to 'full'.
+        """
+        output = "full" if output is None else output
+        if output not in ["compact", "full"]:
+            output_warning(output)
+            output = 'full'
+
+        if adjusted:
+            data, metadata = self._time_series.get_monthly_adjusted(
+                self.ticker, outputsize=output)
+        else:
+            data, metadata = self._time_series.get_monthly(
+                self.ticker, outputsize=output)
+        suffix = "" if not adjusted else "Adjusted"
+        data.to_csv(
+            self.pathway+f"/{self.ticker}_Monthly{suffix}", index=False)
--- tests/unit/test_investigate/test_inspector.py
+++ tests/unit/test_investigate/test_inspector.py
@@ -3,9 +3,19 @@ import warnings
 
 from qutrade.investigate._inspector import output_warning
 from qutrade.investigate import Inspect
+from qutrade.types import Ticker
 
 
 @pytest.mark.parametrize("arg", ["X", 4, "aaaaa"])
 def test_output_warning(arg):
     with pytest.warns(UserWarning, match=f"Output type specified incorrectly: {arg}. Must be on of 'compact' or 'full', defaulting to 'full'."):
         output_warning(arg)
+
+
+class TestInspect:
+    @pytest.mark.parametrize("ticker", ["IBM", "GOOG", "MSFT"])
+    def test_init(self, ticker):
+        ticker = Ticker(ticker)
+        inspect = Inspect(ticker)
+        assert inspect.ticker == ticker
+        assert inspect.pathway == f"qutrade/data/{ticker}/"
