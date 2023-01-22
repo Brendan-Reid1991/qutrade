297a59129381583ebb12a4da85a23614af26017c
brendan-reid91 <brendan.reid1991@gmail.com>
2023-01-22 15:09:14 +0000

    Add tests for access

---
+++ tests/unit/test_types/test_access.py
@@ -0,0 +1,15 @@
+import pytest
+from pathlib import Path
+
+from qutrade.types import api, Ticker
+
+
+class TestAPI:
+    def test_default_pathway_returns_str(self):
+        assert isinstance(api(), str)
+
+    def test_no_filetype_returns_str(self):
+        assert isinstance(api(Path('tests/data/key')), str)
+
+    def test_pkl_returns_str(self):
+        assert isinstance(api(Path('tests/data/key.pkl')), str)
