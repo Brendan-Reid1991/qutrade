f08382aa560b0b7d1a852479b07b4730c39bb594
brendan-reid91 <brendan.reid1991@gmail.com>
2023-01-29 16:57:37 +0000

    Add tests; mark some files for deletion

--- qutrade/investigate/_inspector.py
+++ qutrade/investigate/_inspector.py
@@ -110,18 +110,13 @@ class Inspect:
 
         return data
 
-    def weekly(
-        self, adjusted: bool = True, output: str = "full", save: bool = False
-    ) -> pd.DataFrame:
+    def weekly(self, adjusted: bool = True, save: bool = False) -> pd.DataFrame:
         """Get the weekly data and save it to a CSV file.
 
         Parameters
         ----------
         adjusted : bool, optional
             Whether or not to access the adjusted daily data.
-        output : str, optional
-            How much data to take, accepted values are 'compact' or 'full'.
-            By default None
         save : bool, optional
             Whether or not to save the data to a CSV, by default False.
 
@@ -133,33 +128,21 @@ class Inspect:
         Warning
             If the output type is not one of 'compact' or 'full', defaults to 'full'.
         """
-        output = "full" if output is None else output
-        if output not in ["compact", "full"]:
-            output_warning(output)
-            output = "full"
-
         if adjusted:
-            data, metadata = self._time_series.get_weekly_adjusted(
-                self.ticker, outputsize=output
-            )
+            data, metadata = self._time_series.get_weekly_adjusted(self.ticker)
         else:
-            data, metadata = self._time_series.get_weekly(
-                self.ticker, outputsize=output
-            )
+            data, metadata = self._time_series.get_weekly(self.ticker)
         data = data.reset_index()
         suffix = "" if not adjusted else "Adjusted"
         data.to_csv(self.pathway + f"/{self.ticker}_Weekly{suffix}", index=False)
 
-    def monthly(self, adjusted: bool = True, output: str = "full", save: bool = False):
+    def monthly(self, adjusted: bool = True, save: bool = False):
         """Get the monthly data and save it to a CSV file.
 
         Parameters
         ----------
         adjusted : bool, optional
             Whether or not to access the adjusted daily data.
-        output : str, optional
-            How much data to take, accepted values are 'compact' or 'full'.
-            By default None
 
         Raises
         ------
@@ -169,19 +152,11 @@ class Inspect:
         Warning
             If the output type is not one of 'compact' or 'full', defaults to 'full'.
         """
-        output = "full" if output is None else output
-        if output not in ["compact", "full"]:
-            output_warning(output)
-            output = "full"
 
         if adjusted:
-            data, metadata = self._time_series.get_monthly_adjusted(
-                self.ticker, outputsize=output
-            )
+            data, metadata = self._time_series.get_monthly_adjusted(self.ticker)
         else:
-            data, metadata = self._time_series.get_monthly(
-                self.ticker, outputsize=output
-            )
+            data, metadata = self._time_series.get_monthly(self.ticker)
         data = data.reset_index()
         suffix = "" if not adjusted else "Adjusted"
         data.to_csv(self.pathway + f"/{self.ticker}_Monthly{suffix}", index=False)
--- tests/unit/test_investigate/test_inspector.py
+++ tests/unit/test_investigate/test_inspector.py
@@ -1,6 +1,8 @@
 import pytest
 import warnings
 
+import pandas as pd
+
 from qutrade.investigate._inspector import output_warning
 from qutrade.investigate import Inspect
 from qutrade.types import Ticker
@@ -8,7 +10,10 @@ from qutrade.types import Ticker
 
 @pytest.mark.parametrize("arg", ["X", 4, "aaaaa"])
 def test_output_warning(arg):
-    with pytest.warns(UserWarning, match=f"Output type specified incorrectly: {arg}. Must be on of 'compact' or 'full', defaulting to 'full'."):
+    with pytest.warns(
+        UserWarning,
+        match=f"Output type specified incorrectly: {arg}. Must be on of 'compact' or 'full', defaulting to 'full'.",
+    ):
         output_warning(arg)
 
 
@@ -19,3 +24,48 @@ class TestInspect:
         inspect = Inspect(ticker)
         assert inspect.ticker == ticker
         assert inspect.pathway == f"qutrade/data/{ticker}/"
+
+    @pytest.fixture(scope="class")
+    def IBM(self):
+        return Inspect(Ticker("IBM"))
+
+    def test_intraday_return_type(self, IBM):
+        assert isinstance(IBM.intraday(), pd.DataFrame)
+
+    @pytest.mark.parametrize("interval", list(range(0, 100)))
+    def test_intraday_interval_error(self, IBM, interval):
+        if interval not in [1, 5, 15, 30, 60]:
+            with pytest.raises(ValueError, match="Incorrect interval value given"):
+                IBM.intraday(interval=interval)
+
+    def test_intraday_output_warning(self, IBM):
+        with pytest.warns(UserWarning):
+            IBM.intraday(output="blarg")
+
+    def test_daily_adjusted_return_type(self, IBM):
+        assert isinstance(IBM.daily(adjusted=True), pd.DataFrame)
+
+    def test_daily_adjusted_csv_headers(self, IBM):
+        assert any("adjusted" in x for x in IBM.daily(adjusted=True).columns)
+
+    @pytest.mark.xfail()
+    def test_daily_unadjusted_return_type(self, IBM):
+        assert isinstance(IBM.daily(adjusted=False), pd.DataFrame)
+
+    @pytest.mark.xfail()
+    def test_daily_unadjusted_csv_headers(self, IBM):
+        assert any("adjusted" in x for x in IBM.daily(adjusted=False).columns)
+
+    def test_weekly_adjusted_return_type(self, IBM):
+        assert isinstance(IBM.weekly(adjusted=True), pd.DataFrame)
+
+    def test_weekly_adjusted_csv_headers(self, IBM):
+        assert any("adjusted" in x for x in IBM.weekly(adjusted=True).columns)
+
+    @pytest.mark.xfail()
+    def test_weekly_unadjusted_return_type(self, IBM):
+        assert isinstance(IBM.weekly(adjusted=False), pd.DataFrame)
+
+    @pytest.mark.xfail()
+    def test_weekly_unadjusted_csv_headers(self, IBM):
+        assert any("adjusted" in x for x in IBM.weekly(adjusted=False).columns)
