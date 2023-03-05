93441a1583ed0d930a5a6a34f84abbd5de2701cf
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2023-03-05 13:15:27 +0000

    Deletion

--- qutrade/experiments/test_file.py
+++
@@ -1,8 +0,0 @@
-from qutrade.investigate import Inspect
-from qutrade.types import Ticker
-
-ibm = Inspect(Ticker("IBM"))
-
-data = ibm.intraday(save=True)
-
-# print(data.index.get_level_values("date"))
